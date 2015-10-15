da870d1e4c63c880589db544c367a693cef94b62
MZ1994 <z.macej@gmail.com>
2015-10-15 14:57:46 +0200

    Piersza dzialajaca wersja klasy Produkt V1.0

--- product.rb
+++ product.rb
@@ -48,19 +48,19 @@ class Product
 end
 
 
-a=Product.new("Oliwki","16 kg","25,00 $")
-b=Product.new("Zeszyt","25 szt","5,50 $")
-c=Product.new("Error","13 szt","6,66 $")
-
-a.add_category("Jedzenie")
-b.add_category("Nauka")
-b.add_category("Sztuka")
-c.add_category("eRRor")
-c.add_category("BlAd")
-c.add_category("13")
-a.show_categories
-a.show
-b.show_categories
-b.show
-c.show_categories
-c.show
+#	a=Product.new("Oliwki","16 kg","25,00 $")
+#	b=Product.new("Zeszyt","25 szt","5,50 $")
+#	c=Product.new("Error","13 szt","6,66 $")
+#
+#	a.add_category("Jedzenie")
+#	b.add_category("Nauka")
+#	b.add_category("Sztuka")
+#	c.add_category("eRRor")
+#	c.add_category("BlAd")
+#	c.add_category("13")
+#	a.show_categories
+#	a.show
+#	b.show_categories
+#	b.show
+#	c.show_categories
+# 	c.show
